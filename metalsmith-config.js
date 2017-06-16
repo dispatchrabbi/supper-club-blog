0ae92ba682cf5f13c62e1dec9b8885bde2f6efea
Eli Morris-Heft <eli.morris.heft@gmail.com>
2017-06-16 00:24:59 -0500

    Put home page placeholder content and initial styles in

--- metalsmith-config.js
+++ metalsmith-config.js
@@ -1,19 +1,29 @@
+const bourbon = require('bourbon');
+const neat = require('bourbon-neat');
+
 const METALSMITH_CONFIG = {
-  source: 'src',
-  destination: 'dist',
+  source: 'src/',
+  destination: 'dist/',
   metadata: {
     blogTitle: 'Supper Club Reports',
     description: 'Two foodies eat their way through Chicagoland',
   },
   plugins: {
+    'metalsmith-sass': {
+      includePaths: [].concat(bourbon.includePaths, neat.includePaths),
+      outputDir: 'styles/',
+      outputStyle: 'nested',
+      sourceMap: true,
+      sourceMapContents: true,
+      sourceComments: true,
+    },
     'metalsmith-drafts': true,
     'metalsmith-markdown': true,
     'metalsmith-layouts': {
-      engine: 'handlebars'
+      engine: 'handlebars',
+      partials: 'partials',
     },
-    'metalsmith-permalinks': {
-      pattern: ':title'
-    },
+    'metalsmith-permalinks': {},
   }
 };
 
---
+++ src/posts/sarkis.md
@@ -0,0 +1,8 @@
+---
+title: Sarkis
+date: 2017-06-04
+layout: default.html
+---
+Sarkis is wonderful. Everyone should eat there.
+
+*Yeah!*
