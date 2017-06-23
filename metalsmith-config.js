014bcdeaa0b70888ebe2bc14f1b79b21c84beda3
Eli Morris-Heft <eli.morris.heft@gmail.com>
2017-06-22 20:36:03 -0500

    Make restaurants-to-visit list dynamic

--- metalsmith-config.js
+++ metalsmith-config.js
@@ -1,13 +1,12 @@
+const metadata = require('./metadata/metadata.js');
+
 const bourbon = require('bourbon');
 const neat = require('bourbon-neat');
 
 const METALSMITH_CONFIG = {
   source: 'src/',
   destination: 'dist/',
-  metadata: {
-    blogTitle: 'Supper Club Reports',
-    description: 'Two foodies eat their way through Chicagoland',
-  },
+  metadata,
   plugins: {
     'metalsmith-sass': {
       includePaths: [].concat(bourbon.includePaths, neat.includePaths),
