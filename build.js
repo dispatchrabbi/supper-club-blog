81ccf633e9af23984e4cb9aeda5b01fa468145ec
Eli Morris-Heft <eli@jellyvision.com>
2017-06-23 18:48:28 -0500

    Add collections build step

--- build.js
+++ build.js
@@ -2,10 +2,12 @@ const clean = require('./lib/build/clean.js');
 const prepHandlebars = require('./lib/build/prep-handlebars.js');
 const compileLayouts = require('./lib/build/compile-layouts.js');
 const parseSourceFiles = require('./lib/build/parse-source-files.js');
+const collectCollections = require('./lib/build/collect-collections.js');
 const removeDrafts = require('./lib/build/remove-drafts.js');
 const composeSourceAndLayouts = require('./lib/build/compose-source-and-layouts.js');
 const writeFiles = require('./lib/build/write-files.js');
 const compileStyles = require('./lib/build/compile-styles.js');
+const copyOtherFiles = require('./lib/build/copy-other-files.js');
 
 const helpers = require('./helpers/helpers.js');
 const metadata = require('./metadata/metadata.js');
@@ -13,7 +15,7 @@ const metadata = require('./metadata/metadata.js');
 const SRCDIR = './src/';
 const DESTDIR = './dist/';
 
-// TODO: collections, permalinks
+// TODO: permalinks
 // TODO: logging/debugging
 async function build() {
   await clean(DESTDIR);
@@ -23,35 +25,64 @@ async function build() {
     helpers,
     partialsDir: './partials/',
   });
+  const Handlebars = require('handlebars');
 
+  // Compile layouts for later use
   const layouts = await compileLayouts({
     layoutsDir: './layouts/',
   });
 
+  // Extract front-matter data and turn Markdown into HTML
   const sourceFiles = await parseSourceFiles({
     sourceDir: SRCDIR,
   });
 
+  const POSTS_REGEX = /posts/;
+  const collections = collectCollections({
+    sourceFiles,
+    patterns: [{
+      name: 'posts',
+      test: file => POSTS_REGEX.test(file.srcpath),
+    }],
+    layouts,
+    metadata,
+  });
+  Object.assign(metadata, {collections});
+
+  // Smash layout templates and source pages together to make actual pages
   const finalFiles = composeSourceAndLayouts({
     sourceFiles: removeDrafts(sourceFiles),
     layouts,
     metadata,
   });
 
+  // Write the smashed files to the destination directory
   await writeFiles({
     destinationDir: DESTDIR,
-    files: finalFiles,
+    files: finalFiles.reduce(function(obj, file) {
+      obj[file.dstpath] = file.content;
+      return obj;
+    }, {}),
   });
 
+  // Compile SCSS into CSS (with sourcemaps!)
   const styleFiles = await compileStyles({
     stylesDir: SRCDIR + 'styles/',
     outputDir: DESTDIR + 'styles/',
   });
 
+  // Write the CSS to the destination directory
   await writeFiles({
     destinationDir: DESTDIR + 'styles/',
     files: styleFiles
   });
+
+  // We have no other files we need to copy... yet
+  // await copyOtherFiles({
+  //   sourceDir: ['images/'].map(dir => SRCDIR + dir),
+  //   exclude: [],
+  //   destinationDir: DESTDIR,
+  // });
 }
 
 build();
