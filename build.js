bf0d00c069e1a5893666143b4f18727a7af245d7
Eli Morris-Heft <eli.morris.heft@gmail.com>
2017-07-08 19:29:28 -0500

    Restructure build process so collections work nicer

--- build.js
+++ build.js
@@ -1,92 +1,116 @@
-const clean = require('./lib/build/clean.js');
+const path = require('path');
+const del = require('del');
+const Handlebars = require('handlebars');
+
+const collectAndProcessFiles = require('./lib/build/collect-and-process-files.js');
+const mapObject = require('./lib/map-object.js');
+
 const prepHandlebars = require('./lib/build/prep-handlebars.js');
-const compileLayouts = require('./lib/build/compile-layouts.js');
-const parseSourceFiles = require('./lib/build/parse-source-files.js');
-const collectCollections = require('./lib/build/collect-collections.js');
+const demarkdownifyFile = require('./lib/build/demarkdownify-file.js');
 const removeDrafts = require('./lib/build/remove-drafts.js');
-const composeSourceAndLayouts = require('./lib/build/compose-source-and-layouts.js');
-const { fancifyUrls } = require('./lib/build/fancify-urls.js');
+const curateCollection = require('./lib/build/curate-collection.js');
+const composeFile = require('./lib/build/compose-file.js');
 const writeFiles = require('./lib/build/write-files.js');
 const compileStyles = require('./lib/build/compile-styles.js');
 const copyOtherFiles = require('./lib/build/copy-other-files.js');
 
-const helpers = require('./helpers/helpers.js');
-const metadata = require('./metadata/metadata.js');
+// TODO: logging, debugging, allow handlebar stuff in the markdown
+// Note to future self: allowing handlebar stuff means breaking apart demarkdownify
 
-const SRCDIR = './src/';
-const DESTDIR = './dist/';
+DEFAULT_OPTIONS = {
+  includeDrafts: false,
+  helpers: require('./helpers/helpers.js'),
+  partialsDir: './partials/',
+  layoutsDir: './layouts/',
+  metadata: require('./metadata/metadata.js'),
+};
 
-// TODO: logging/debugging
-async function build(includeDrafts = false) {
-  await clean(DESTDIR);
+async function build(srcDir, destDir, options) {
+  const opts = Object.assign({}, DEFAULT_OPTIONS, options);
 
-  // Register helpers and partials
-  await prepHandlebars({
-    helpers,
-    partialsDir: './partials/',
-  });
-  const Handlebars = require('handlebars');
+  // Clean destination directory
+  await del([destDir + '**']);
 
-  // Compile layouts for later use
-  const layouts = await compileLayouts({
-    layoutsDir: './layouts/',
-  });
-
-  // Extract front-matter data and turn Markdown into HTML
-  const sourceFiles = await parseSourceFiles({
-    sourceDir: SRCDIR,
+  // Add partials and helpers to Handlebars
+  prepHandlebars({
+    helpers: opts.helpers,
+    partials: await collectAndProcessFiles('*.html', opts.partialsDir),
   });
 
+  // Create an index of files to process
+  const fileIndex = await collectAndProcessFiles('**/*.md', srcDir, (content, filepath) => {
+    const pathParts = path.parse(filepath);
+
+    return {
+      srcPath: filepath,
+      dstPath: path.format({
+        dir: pathParts.dir,
+        name: pathParts.name,
+        ext: '.html',
+      }),
+      slug: pathParts.name,
+      rawContent: content,
+    };
+  });
+
+  // Prettify all the file URLs (except index.html, which is already pretty)
+  mapObject(fileIndex, listing => {
+    listing.dstPath = listing.dstPath === 'index.html' ?
+      listing.dstPath :
+      listing.dstPath.replace('.html', '/index.html');
+    return listing;
+  });
+
+  // Process the front-matter of those files and convert the content to HTML
+  mapObject(fileIndex, (listing, path) => demarkdownifyFile(listing));
+
+  // Remove drafts from the index
+  if(!opts.includeDrafts) {
+    removeDrafts(fileIndex);
+  }
+
+  // Pull out collections from the index and
+  // make those collections available in the metadata
   const POSTS_REGEX = /^posts\//;
-  const collections = collectCollections({
-    sourceFiles,
-    patterns: [{
-      name: 'posts',
-      test: file => POSTS_REGEX.test(file.srcpath),
-    }],
-    layouts,
-    metadata,
-    includeDrafts,
-  });
-  Object.assign(metadata, {collections});
+  options.metadata.collections = {
+    posts: curateCollection(fileIndex, path => POSTS_REGEX.test(path)),
+  };
 
-  // Smash layout templates and source pages together to make actual pages
-  // And also fancify the URLs so they look nice on the URL bar
-  const finalFiles = fancifyUrls(composeSourceAndLayouts({
-    sourceFiles: includeDrafts ? sourceFiles : removeDrafts(sourceFiles),
-    layouts,
-    metadata,
-  }));
+  // Compile layout templates
+  const layouts = await collectAndProcessFiles('*.html', opts.layoutsDir, (content, filepath) => Handlebars.compile(content))
 
-  // Write the smashed files to the destination directory
+  // Compose the content, layout, and metadata of each file to
+  // create the full page HTML
+  mapObject(fileIndex, listing => composeFile(listing, layouts, options.metadata));
+
+  // Write those files to the destination directory
   await writeFiles({
-    destinationDir: DESTDIR,
-    files: finalFiles.reduce(function(obj, file) {
-      obj[file.dstpath] = file.content;
-      return obj;
-    }, {}),
+    destinationDir: destDir,
+    files: Object.values(fileIndex).reduce((obj, listing) => (obj[listing.dstPath] = listing.pageContent, obj), {}),
   });
 
-  // Compile SCSS into CSS (with sourcemaps!)
+  // Compile styles from the source directory...
   const styleFiles = await compileStyles({
-    stylesDir: SRCDIR + 'styles/',
-    outputDir: DESTDIR + 'styles/',
+    stylesDir: srcDir + 'styles/',
+    outputDir: destDir + 'styles/',
   });
 
-  // Write the CSS to the destination directory
+  // ...and write them to the destination directory
   await writeFiles({
-    destinationDir: DESTDIR + 'styles/',
+    destinationDir: destDir + 'styles/',
     files: styleFiles
   });
 
-  // We have no other files we need to copy... yet
+  // Copy any remaining files from the source directory to the destination directory
+  // (We have no other files we need to copy... yet.)
   // await copyOtherFiles({
-  //   sourceDir: ['images/'].map(dir => SRCDIR + dir),
+  //   sourceDir: ['images/'].map(dir => srcDir + dir),
   //   exclude: [],
-  //   destinationDir: DESTDIR,
+  //   destinationDir: destDir,
   // });
+
 }
 
-build(
-  process.env.INCLUDE_DRAFTS === 'true' // includeDrafts
-);
+build('./src/', './dist/', {
+  includeDrafts: (process.env.INCLUDE_DRAFTS === 'true'),
+});
